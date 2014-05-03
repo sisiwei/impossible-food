6df3637fa7a12077b2f7c2a7b215697b08a72981
Sisi Wei <me@sisiwei.com>
2014-05-03 18:05:58 -0400

    Drop down is functional.

--- js/script.js
+++ js/script.js
@@ -5,10 +5,26 @@ $(document).ready(function(){
     for (var food_category in data) {
       for (var food_id in data[food_category]) {
         var food = data[food_category][food_id];
-        $("#food").append('<option value="'+ food_id +'">'+ food + '</option>');
+        $("#food").append('<option value="'+ food_id +'" data-food=' + food + '>'+ food + '</option>');
         $("#food").trigger("chosen:updated");
       }
     }
+  });
+
+  $('.add-to-list').click(function(){
+  	// Detect the values in the select boxes
+  	var foodID = $('#food').val();
+  	var foodName = $("#food option:selected").text();
+  	var qty = $('#qty').val() == "" ? "1" : $('#qty').val();
+
+  	// Calculate the food miles TBD
+  	var foodMiles = "2,345";
+
+  	// Add a line into the .table below
+  	var text = '<div class="tr just-loaded cf"><div class="qty-col">' + qty + '</div> <div class="food-item"><span>' + foodName + ' <i class="fa fa-times"></i></span></div> <div class="food-miles">' + foodMiles + '</div> </div>';
+  	$('.table .th').after(text);
+  	$('.just-loaded').fadeIn(1000);
+  	$('.just-loaded').removeClass('.just-loaded');
   });
 
 });
@@ -22,5 +38,4 @@ function distance(lnglat1,lnglat2) {
     //Earth's radius in miles
     return  3963.19 * Math.acos(Math.sin(lat1) * Math.sin(lat2) +
         Math.cos(lat1) * Math.cos(lat2) * Math.cos((lnglat2[0] - lnglat1[0]) * rad));
-
 }
