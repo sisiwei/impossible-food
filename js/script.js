ed01b8b9ce2a2a01d16d71a0c5294b4c513e0ccd
Noah <noah@noahveltman.com>
2014-05-04 00:59:59 -0400

    Fixing quantity to input

--- js/script.js
+++ js/script.js
@@ -25,7 +25,11 @@ $(document).ready(function(){
       $("#food").trigger("chosen:updated");
 
 	  	// Add a line into the .table below
-	  	var text = '<div class="tr just-loaded cf"><div class="qty-col"><span>' + qty + '</span><input type="number" value="' + qty + '"/></div> <div class="food-item"><span>' + foodName + ' <i class="fa fa-times"></i></span></div> <div class="food-miles">' + addCommas(foodMiles) + '</div> </div>';
+	  	var text = $('<div class="tr just-loaded cf"><div class="qty-col"><span>' + qty + '</span><input type="text" pattern="[0-9]*" value="' + qty + '"/></div> <div class="food-item"><span>' + foodName + ' <i class="fa fa-times"></i></span></div> <div class="food-miles">' + addCommas(foodMiles) + '</div> </div>');
+      text.find(".qty-col").on("click",function(e){
+        $(this).addClass("active");
+
+      });
 	  	$('.table .th').after(text);
 	  	$('.just-loaded').fadeIn(1000);
 	  	$('.just-loaded').removeClass('.just-loaded');
